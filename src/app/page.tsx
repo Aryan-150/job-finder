431667296dedb51242f0763a804029e3d35aeaaf
Aryan Bachchu <aryanbachchu150@gmail.com>
2025-08-26 13:36:43 +0530

    added button.tsx:V1 workable

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,33 @@
+"use client";
+
 import { cn } from "@/lib/utils";
+import Button from "@/ui/button";
 
 export default function Home() {
   return (
     <div className={cn(
-      "bg-blue-600"
+      "h-screen p-2 flex flex-col gap-5"
     )}>
-      hello world...! 
+      <div className="flex flex-col items-start gap-5">
+        <Button variant={"primary"} size={"xs"} text="Log in" />
+        <Button variant={"primary"} size={"xs"} text="Log in" disabled />
+        <Button variant={"primary"} size={"sm"} text="Log in" />
+        <Button variant={"primary"} size={"sm"} text="Log in" isLoading={true} />
+        <Button variant={"primary"} size={"md"} text="Log in" />
+        <Button variant={"primary"} size={"md"} text="Log in" isLoading={true} />
+        <Button variant={"primary"} size={"lg"} text="Log in" />
+        <Button variant={"primary"} size={"lg"} text="Log in" disabled />
+      </div>
+      <div className="flex flex-col items-start gap-5">
+        <Button variant={"secondary"} size={"xs"} text="Log in" />
+        <Button variant={"secondary"} size={"xs"} text="Log in" disabled />
+        <Button variant={"secondary"} size={"sm"} text="Log in" />
+        <Button variant={"secondary"} size={"sm"} text="Log in" isLoading={true} />
+        <Button variant={"secondary"} size={"md"} text="Log in" />
+        <Button variant={"secondary"} size={"md"} text="Log in" isLoading={true} />
+        <Button variant={"secondary"} size={"lg"} text="Log in" />
+        <Button variant={"secondary"} size={"lg"} text="Log in" disabled />
+      </div>
     </div>
   )
 }
--- src/ui/button.tsx
+++ src/ui/button.tsx
@@ -2,44 +2,62 @@ import { cn } from "@/lib/utils";
 import { cva, VariantProps } from "class-variance-authority";
 import React, { ButtonHTMLAttributes } from "react";
 
-const buttonDefaultStyles = "cursor-pointer transition-all duration-300";
+const buttonDefaultStyles = "h-fit w-fit tracking-tight cursor-pointer transition-all duration-300 text-center";
 const buttonStyles = cva(buttonDefaultStyles, {
   variants: {
     variant: {
-      "primary": "",
-      "secondary": ""
+      primary: "bg-honey-500 text-white border border-honey-800/75 hover:ring-4 hover:ring-honey-100 ring-offset-honey-50 hover:ring-offset-1",
+      secondary: "bg-honey-50 text-honey-600 border border-honey-600/75 hover:ring-4 hover:ring-honey-100 ring-offset-honey-50 hover:ring-offset-1",
+      disabled: "opacity-75 cursor-not-allowed bg-gray-200 hover:bg-gray-300"
     },
     size: {
-      "sm": "",
-      "md": "",
-      "lg": ""
+      xs: "py-1.5 px-7 text-base font-medium tex-shadow-xs rounded-lg shadow-xs",
+      sm: "py-2 px-10 text-lg font-semibold text-shadow-xs rounded-lg shadow-xs",
+      md: "py-2.5 px-15 text-2xl font-semibold text-shadow-xs rounded-xl shadow-sm",
+      lg: "py-2.5 px-17.5 text-2xl font-semibold text-shadow-xs rounded-xl shadow-sm",
     }
+  },
+  compoundVariants: [
+    {
+      variant: ["primary"],
+      className: "text-shadow-honey-600 shadow-honey-800/75"
+    },
+    {
+      variant: ["secondary"],
+      className: "text-shadow-honey-100 shadow-honey-600/75"
+    }
+  ],
+  defaultVariants: {
+    variant: "primary",
+    size: "md"
   }
 })
 
-interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>, VariantProps<typeof buttonStyles> {
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>, Required<VariantProps<typeof buttonStyles>> {
   text: string;
-  isDisabled: boolean;
+  isLoading?: boolean;
   onClick ?: () => void;
 }
 
 export default function Button({
   variant,
   size,
+  isLoading,
   text,
   onClick,
-  isDisabled,
   className,
   ...props
 }: ButtonProps) {
   return (
     <button {...props} 
       className={cn(
-        buttonStyles({variant, size}),
+        props.disabled || isLoading
+        ? buttonStyles({variant: "disabled", size})
+        : buttonStyles({variant, size}),
         className
       )}
       onClick={onClick}
-      disabled={isDisabled}
+      disabled={props.disabled || isLoading}
     >
       {text}
     </button>
