0725e45553306842547a4347171a8ebe1e7138a8
Aryan Bachchu <aryanbachchu150@gmail.com>
2025-09-05 10:56:51 +0530

    feat: added shadCN and lucide-react, also configured it with dark & light mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,9 @@
-"use client";
+import HomePage from "@/components/home";
 
-import { cn } from "@/lib/utils";
-import Button from "@/ui/button";
+export default async function Home() {
 
-export default function Home() {
+  await new Promise(r => setTimeout(r,2000));
   return (
-    <div className={cn(
-      "h-screen p-2 flex flex-col gap-5"
-    )}>
-      <div className="flex flex-col items-start gap-5">
-        <Button variant={"primary"} size={"xs"} text="Log in" />
-        <Button variant={"primary"} size={"xs"} text="Log in" disabled />
-        <Button variant={"primary"} size={"sm"} text="Log in" />
-        <Button variant={"primary"} size={"sm"} text="Log in" isLoading={true} />
-        <Button variant={"primary"} size={"md"} text="Log in" />
-        <Button variant={"primary"} size={"md"} text="Log in" isLoading={true} />
-        <Button variant={"primary"} size={"lg"} text="Log in" />
-        <Button variant={"primary"} size={"lg"} text="Log in" disabled />
-      </div>
-      <div className="flex flex-col items-start gap-5">
-        <Button variant={"secondary"} size={"xs"} text="Log in" />
-        <Button variant={"secondary"} size={"xs"} text="Log in" disabled />
-        <Button variant={"secondary"} size={"sm"} text="Log in" />
-        <Button variant={"secondary"} size={"sm"} text="Log in" isLoading={true} />
-        <Button variant={"secondary"} size={"md"} text="Log in" />
-        <Button variant={"secondary"} size={"md"} text="Log in" isLoading={true} />
-        <Button variant={"secondary"} size={"lg"} text="Log in" />
-        <Button variant={"secondary"} size={"lg"} text="Log in" disabled />
-      </div>
-    </div>
+    <HomePage />
   )
 }
---
+++ src/components/home.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { useTheme } from "next-themes";
+
+export default function HomePage() {
+  const { theme, setTheme } = useTheme();
+
+  return (
+    <div className={cn(
+      "h-screen w-screen flex flex-col justify-start items-center",
+      "bg-gradient-to-l from-honey-700/70 via-white to-white",
+      "dark:bg-gradient-to-l dark:from-honey-700/50 from-5% dark:via-[#0b0b0b] dark:to-[#0b0b0b]"
+    )}>
+      <div className={cn(
+        "w-full max-w-6xl mx-auto h-fit px-10 py-2 flex justify-between items-center bg-transparent"
+      )}>
+        <span className="text-3xl font-bold tracking-tight">
+          Job Bee
+        </span>
+        <Button variant={"secondary"} onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>
+          {theme === 'dark' ? 'Switch to light' : 'Switch to dark'}
+        </Button>
+        <Button size={"sm"}>Sign up</Button>
+      </div>
+    </div>
+  );
+}
